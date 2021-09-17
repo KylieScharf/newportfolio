caf7a35a1a6e73a0c7ea0c11b36a5f22a7c7c1a3
1917809 <89220092+1917809@users.noreply.github.com>
2021-09-17 11:13:14 -0700

    made more stub area 51 pages and added the algorithms to a seperate python page

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # import "packages" from flask
 from flask import Flask, render_template, request
-from PIL import Image, ImageDraw
-import numpy
-import base64
-from io import BytesIO
+from algorithm import image_data
+
 
 # create a Flask instance
 app = Flask(__name__)
@@ -149,6 +147,30 @@ def A51story121():
 def A51story122():
     return render_template("A51stories/A51story122.html")
 
+@app.route('/A51story1121/')
+def A51story1121():
+    return render_template("A51stories/A51story1121.html")
+
+@app.route('/A51story1122/')
+def A51story1122():
+    return render_template("A51stories/A51story1122.html")
+
+@app.route('/A51story1211/')
+def A51story1211():
+    return render_template("A51stories/A51story1211.html")
+
+@app.route('/A51story1212/')
+def A51story1212():
+    return render_template("A51stories/A51story1212.html")
+
+@app.route('/A51story1221/')
+def A51story1221():
+    return render_template("A51stories/A51story1221.html")
+
+@app.route('/A51story1222/')
+def A51story1222():
+    return render_template("A51stories/A51story1222.html")
+
 
 #BT stories
 @app.route('/BTstory1/')
@@ -220,59 +242,6 @@ def binary():
 @app.route('/rgb/', methods=["GET", "POST"])
 def rgb():
     return render_template('rgb.html', images=image_data())
-
-# image (PNG, JPG) to base64 conversion (string), learn about base64 on wikipedia https://en.wikipedia.org/wiki/Base64
-def image_base64(img, img_type):
-    with BytesIO() as buffer:
-        img.save(buffer, img_type)
-        return base64.b64encode(buffer.getvalue()).decode()
-# formatter preps base64 string for inclusion, ie <img src=[this return value] ... />
-def image_formatter(img, img_type):
-    return "data:image/" + img_type + ";base64," + image_base64(img, img_type)
-# color_data prepares a series of images for data analysis
-
-def image_data(path="static/", img_list=None):  # path of static images is defaulted
-    if img_list is None:  # color_dict is defined with defaults
-        img_list = [
-            {'source': "Romolo Tavani ", 'label': "Drowning Hand", 'file': "rip.jpg"}, # this is one dictionary where the source is the word and the "smiley" is the definition
-        ]
-    # gather analysis data and meta data for each image, adding attributes to each row in table
-    for img_dict in img_list:
-        img_dict['path'] = '/' + path  # path for HTML access (frontend)
-        file = path + img_dict['file']  # file with path for local access (backend)
-        # Python Image Library operations
-        img_reference = Image.open(file)  # PIL
-        img_data = img_reference.getdata()  # Reference https://www.geeksforgeeks.org/python-pil-image-getdata/ this gets the binary from the RGB without meta data
-        img_dict['format'] = img_reference.format
-        img_dict['mode'] = img_reference.mode
-        img_dict['size'] = img_reference.size
-        # Conversion of original Image to Base64, a string format that serves HTML nicely
-        img_dict['base64'] = image_formatter(img_reference, img_dict['format'])
-        # Numpy is used to allow easy access to data of image, python list
-        img_dict['data'] = numpy.array(img_data)
-        img_dict['hex_array'] = []
-        img_dict['binary_array'] = []
-        # 'data' is a list of RGB data, the list is traversed and hex and binary lists are calculated and formatted
-        for pixel in img_dict['data']:
-            # hexadecimal conversions
-            hex_value = hex(pixel[0])[-2:] + hex(pixel[1])[-2:] + hex(pixel[2])[-2:]
-            hex_value = hex_value.replace("x", "0")
-            img_dict['hex_array'].append("#" + hex_value)
-            # binary conversions
-            bin_value = bin(pixel[0])[2:].zfill(8) + " " + bin(pixel[1])[2:].zfill(8) + " " + bin(pixel[2])[2:].zfill(8)
-            img_dict['binary_array'].append(bin_value)
-        # create gray scale of image, ref: https://www.geeksforgeeks.org/convert-a-numpy-array-to-an-image/
-        img_dict['gray_data'] = []
-        for pixel in img_dict['data']:
-            average = (pixel[0] + pixel[1] + pixel[2]) // 3
-            if len(pixel) > 3:
-                img_dict['gray_data'].append((average, average, average, pixel[3])) # append means to add it like when you do "" + something to create a string
-            else:
-                img_dict['gray_data'].append((average, average, average))
-        img_reference.putdata(img_dict['gray_data'])
-        img_dict['base64_GRAY'] = image_formatter(img_reference, img_dict['format'])
-    return img_list  # list is returned with all the attributes for each image dictionary
-# run this as standalone tester to see data printed in terminal
 
 
 

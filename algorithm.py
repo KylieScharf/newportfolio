21487fdf9a9bbc4f96c41ff203d844caeed309de
1917809 <89220092+1917809@users.noreply.github.com>
2021-09-19 18:37:44 -0700

    added text to image and edited read me

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import numpy
 import base64
 from io import BytesIO
@@ -7,7 +7,7 @@ from io import BytesIO
 def image_base64(img, img_type):
     with BytesIO() as buffer:
         img.save(buffer, img_type) #this saves the image as something different/converts it into binary for the data
-        return base64.b64encode(buffer.getvalue()).decode()
+        return base64.b64encode(buffer.getvalue()).decode() #after dot is a function
 # formatter preps base64 string for inclusion, ie <img src=[this return value] ... />
 def image_formatter(img, img_type):
     return "data:image/" + img_type + ";base64," + image_base64(img, img_type)
@@ -24,6 +24,9 @@ def image_data(path="static/", img_list=None):  # path of static images is defau
         file = path + img_dict['file']  # file with path for local access (backend)
         # Python Image Library operations
         img_reference = Image.open(file)  # PIL: this is opeening the image like if we did image.open(static/smiley.jpg) adn this creates an instance of the image. image.open() is a function that loads the image and creates an image pbject that can be modified with the .save: the image object we recieve using image.open can later be used to resize, crop, or more the image
+        draw = ImageDraw.Draw(img_reference)
+        #myFont = ImageFont.truetype('E:/PythonPillow/Fonts/FreeMono.ttf', 30)
+        draw.text((75, 20), "APCSP IS COOL", fill=(255, 0, 0))
         img_data = img_reference.getdata()  # Reference https://www.geeksforgeeks.org/python-pil-image-getdata/ this gets the binary from the RGB without meta data :  Image.open() reads the image and gets all relevant info from the image.
         img_dict['format'] = img_reference.format #jpeg vs png (file format)
         img_dict['mode'] = img_reference.mode #pixel format s o RGB vs RGBA
@@ -53,6 +56,27 @@ def image_data(path="static/", img_list=None):  # path of static images is defau
                 img_dict['gray_data'].append((average, average, average))
         img_reference.putdata(img_dict['gray_data'])
         img_dict['base64_GRAY'] = image_formatter(img_reference, img_dict['format'])
+        draw = ImageDraw.Draw(img_reference)
+        draw.text((0, 0), "Size is 600 X 600")  # draw in image
     return img_list  # list is returned with all the attributes for each image dictionary
 # run this as standalone tester to see data printed in terminal
 
+
+if __name__ == "__main__":
+    local_path = "static/"
+    img_test = [
+        {'source': "iconsdb.com", 'label': "Blue square", 'file': "rip.jpg"},
+    ]
+    items = image_data(local_path, img_test)  # path of local run
+    for row in items:
+        # print some details about the image so you can validate that it looks like it is working
+        # meta data
+        print("---- meta data -----")
+        print(row['size'])
+        print("----  render and write in image  -----")
+        filename = local_path + row['file']
+        image_ref = Image.open(filename)
+        draw = ImageDraw.Draw(image_ref)
+        draw.text((0, 0), "Size is {0} X {1}".format(*row['size']))  # draw in image
+        image_ref.show()
+print()
